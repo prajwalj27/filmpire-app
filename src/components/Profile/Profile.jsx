f499b2e0229c22fdb7aae93b0bb4d5191507c993
Prajwal Jaiswal <prajwalmjaiswal@gmail.com>
2022-10-08 17:11:26 +0530

    Add basic Profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Box, Typography, Button } from '@mui/material';
 import { useSelector } from 'react-redux';
+import { ExitToApp } from '@mui/icons-material';
 
 import { userSelector } from '../../features/auth';
 
 function Profile() {
   const { user } = useSelector(userSelector);
+  const favoriteMovies = [];
+
+  const logout = () => {
+    // Clear localstorage
+    localStorage.clear();
+
+    // Reload the page
+    window.location.href = '/';
+  };
 
   return (
-    <div>
-      <h2>Welcome, {user.username}</h2>
-    </div>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length ? (
+        <Typography variant="h5">
+          Add fovorites or watchlist some movies to see them here!
+        </Typography>
+      ) : (
+        <Box>FAVORITE MOVIES</Box>
+      )}
+    </Box>
   );
 }
 
